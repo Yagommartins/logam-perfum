c2e50da5648ab43bc77aff9ffaa3f47a78bc9574
Yago Martins <yagomartins64@gmail.com>
2025-08-07 17:40:21 -0300

    primeiro commit

---
+++ src/components/Topo/index.js
@@ -0,0 +1,17 @@
+export default function Topo () {
+    return (
+        <header>
+            <div>
+                <picture>
+                <a href=""><img src='./logo.jpg'/> </a>
+                </picture>
+
+                <nav>
+                    <a href="#">Produtos</a>
+                    <a href="#">Sobre</a>
+                    <a href="#">Contatos</a>
+                </nav>
+            </div>
+            </header>
+    )
+}
